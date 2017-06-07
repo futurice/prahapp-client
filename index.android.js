860cbfac7e78580ef470f39bf4de88d14c35718b
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-06-07 10:38:43 +0300

    Refactor android index

--- index.android.js
+++ index.android.js
@@ -1,5 +1,7 @@
 'use strict';
-import React, { AppRegistry, UIManager } from 'react-native';
+
+import React from 'react';
+import { AppRegistry, UIManager } from 'react-native';
 import RootView from './app/containers/RootView';
 UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true);
-AppRegistry.registerComponent('prahappclient', () => RootView);
+AppRegistry.registerComponent('futubohemia', () => RootView);
