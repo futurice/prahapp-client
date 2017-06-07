6beadb37a5f07eeaf6029fcf4f0cef8bf50c1448
Pasi Lampinen <plam@dyn-100-68.wlan.futurice.org>
2017-06-07 10:49:43 +0300

    Update static data

--- app/data/events.js
+++ app/data/events.js
@@ -1,198 +1,212 @@
 export default [
 {
-  name: "Lunch",
-  locationName: "Restaurant",
-  startTime: "2017-06-09T10:00:00.000Z",
-  endTime: "2017-06-09T11:00:00.000Z",
-  description: "Bon Appétit",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Coming To Prague",
+  "locationName": "Hilton Prague",
+  "startTime": "2017-06-08T15:35:00.000Z",
+  "endTime": "2017-06-09T07:35:00.000Z",
+  "description": "You arrive to summery Prague in Thursday or Friday. We are staying in Hilton Prague. \n\nHilton Prague\n• 25-minute drive from Airport\n• Central location, within walking distance of main attractions\n• Breakfast\n• Health Club & Spa\n• Three restaurants and two bars\n• Sky Bar & Lounge\n• WiFi in public areas\n\n\nBreakfast in Hotel\n6:00 – 10:30 on FRI\n06:00 – 11:00 on SAT and SUN\n\nGym & Spa\nWe have free access to Hilton Prague Gym and Spa. Amenities include an indoor pool, whirlpool, sauna, steam room, and fitness center.",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.093277",
+    "longitude": "14.4376183"
   },
-  coverImage: "https://static1.squarespace.com/static/53162615e4b08f2413caa7c2/t/571da617d51cd4f1ca29ec3d/1470308449044/traditional-czech-food?format=1500w"
+  "coverImage": "https://media-cdn.tripadvisor.com/media/photo-s/0a/f8/12/68/hilton-prague-exterior.jpg"
 },
 {
-  name: "Welcome Words",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T12:00:00.000Z",
-  endTime: "2017-06-09T12:15:00.000Z",
-  description: "Fututrip 2017",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Lunch",
+  "locationName": "La Fabrika - Restaurant",
+  "startTime": "2017-06-09T09:00:00.000Z",
+  "endTime": "2017-06-09T10:00:00.000Z",
+  "description": "We are starting with lunch.",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://raw.githubusercontent.com/futurice/prahapp-client/master/assets/prague/futubohemia/chilicorn.png"
+  "coverImage": "https://static1.squarespace.com/static/53162615e4b08f2413caa7c2/t/571da617d51cd4f1ca29ec3d/1470308449044/traditional-czech-food?format=1500w"
 },
 {
-  name: "Keynote by Mark Gallagher",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T12:15:00.000Z",
-  endTime: "2017-06-09T13:15:00.000Z",
-  description: "Keynote by Mark Gallagher",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Welcome Words",
+  "locationName": "La Fabrika - Main Stage",
+  "startTime": "2017-06-09T11:00:00.000Z",
+  "endTime": "2017-06-09T11:15:00.000Z",
+  "description": "Fututrip 2017",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://i.ytimg.com/vi/FCQ_sPodSwA/maxresdefault.jpg"
+  "coverImage": "https://raw.githubusercontent.com/futurice/prahapp-client/master/assets/prague/futubohemia/chilicorn.png"
 },
 {
-  name: "Change to workshops",
-  locationName: "Workshops",
-  startTime: "2017-06-09T13:15:00.000Z",
-  endTime: "2017-06-09T13:30:00.000Z",
-  description: "We are starting workshops",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Keynote by Mark Gallagher",
+  "locationName": "La Fabrika - Main Stage",
+  "startTime": "2017-06-09T11:15:00.000Z",
+  "endTime": "2017-06-09T12:15:00.000Z",
+  "description": "Keynote by Mark Gallagher",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://flockler.com/thumbs/1992/20130607-dsc_03641_s830x0_q80_noupscale.jpg"
+  "coverImage": "https://i.ytimg.com/vi/FCQ_sPodSwA/maxresdefault.jpg"
 },
 {
-  name: "Case Studies 1",
-  locationName: "Workshops",
-  startTime: "2017-06-09T13:30:00.000Z",
-  endTime: "2017-06-09T14:15:00.000Z",
-  description: "Workshopping",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Change to workshops",
+  "locationName": "La Fabrika - Workshops",
+  "startTime": "2017-06-09T12:15:00.000Z",
+  "endTime": "2017-06-09T12:30:00.000Z",
+  "description": "We are starting workshops",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://images.unsplash.com/1/bag-and-hands.jpg?dpr=1&auto=format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://flockler.com/thumbs/1992/20130607-dsc_03641_s830x0_q80_noupscale.jpg"
 },
 {
-  name: "Break",
-  locationName: "",
-  startTime: "2017-06-09T14:15:00.000Z",
-  endTime: "2017-06-09T14:30:00.000Z",
-  description: "Small break",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Case Studies 1",
+  "locationName": "La Fabrika - Workshops",
+  "startTime": "2017-06-09T12:30:00.000Z",
+  "endTime": "2017-06-09T13:15:00.000Z",
+  "description": "Workshopping",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://images.unsplash.com/photo-1494398138790-6f0b412da3e6?dpr=1&auto=format&fit=crop&w=1199&h=879&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://images.unsplash.com/1/bag-and-hands.jpg?dpr=1&auto=format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop=&bg="
 },
 {
-  name: "Case Studies 2",
-  locationName: "Workshops",
-  startTime: "2017-06-09T14:30:00.000Z",
-  endTime: "2017-06-09T15:15:00.000Z",
-  description: "Workshopping continues",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Break",
+  "locationName": "La Fabrika - ",
+  "startTime": "2017-06-09T13:15:00.000Z",
+  "endTime": "2017-06-09T13:30:00.000Z",
+  "description": "Small break",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://images.unsplash.com/1/bag-and-hands.jpg?dpr=1&auto=format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://images.unsplash.com/photo-1494398138790-6f0b412da3e6?dpr=1&auto=format&fit=crop&w=1199&h=879&q=80&cs=tinysrgb&crop=&bg="
 },
 {
-  name: "Change to Main Stage",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T15:15:00.000Z",
-  endTime: "2017-06-09T15:30:00.000Z",
-  description: "Let's move it to main stage",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Case Studies 2",
+  "locationName": "La Fabrika - Workshops",
+  "startTime": "2017-06-09T13:30:00.000Z",
+  "endTime": "2017-06-09T14:15:00.000Z",
+  "description": "Workshopping continues",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://flockler.com/thumbs/1992/20130607-dsc_03641_s830x0_q80_noupscale.jpg"
+  "coverImage": "https://images.unsplash.com/1/bag-and-hands.jpg?dpr=1&auto=format&fit=crop&w=1199&h=803&q=80&cs=tinysrgb&crop=&bg="
 },
 {
-  name: "Reservation",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T15:30:00.000Z",
-  endTime: "2017-06-09T16:00:00.000Z",
-  description: "TBA",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Change to Main Stage",
+  "locationName": "La Fabrika - Main Stage",
+  "startTime": "2017-06-09T14:15:00.000Z",
+  "endTime": "2017-06-09T14:30:00.000Z",
+  "description": "Let's move it to main stage",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://media-cdn.tripadvisor.com/media/photo-s/03/9b/2d/fb/prague.jpg"
+  "coverImage": "https://flockler.com/thumbs/1992/20130607-dsc_03641_s830x0_q80_noupscale.jpg"
 },
 {
-  name: "Keynote: LSC next steps",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T15:45:00.000Z",
-  endTime: "2017-06-09T17:00:00.000Z",
-  description: "Keynote: LSC next steps",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Keynote: LSC next steps",
+  "locationName": "La Fabrika - Main Stage",
+  "startTime": "2017-06-09T14:45:00.000Z",
+  "endTime": "2017-06-09T16:00:00.000Z",
+  "description": "Keynote: LSC next steps",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://flockler.com/thumbs/2660/lsc_s1200x630_noupscale.jpg"
+  "coverImage": "https://flockler.com/thumbs/2660/lsc_s1200x630_noupscale.jpg"
 },
 {
-  name: "Instructions",
-  locationName: "Main Stage",
-  startTime: "2017-06-09T17:00:00.000Z",
-  endTime: "2017-06-09T17:15:00.000Z",
-  description: "Instructions for next steps",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Instructions",
+  "locationName": "La Fabrika - Main Stage",
+  "startTime": "2017-06-09T16:00:00.000Z",
+  "endTime": "2017-06-09T16:15:00.000Z",
+  "description": "Instructions for next steps",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.1038408",
+    "longitude": "14.4503703"
   },
-  coverImage: "https://images.unsplash.com/photo-1452690700222-8a2a1a109f4c?dpr=1&auto=compress,format&fit=crop&w=376&h=249&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://images.unsplash.com/photo-1452690700222-8a2a1a109f4c?dpr=1&auto=compress,format&fit=crop&w=376&h=249&q=80&cs=tinysrgb&crop=&bg="
 },
 {
-  name: "Activities",
-  locationName: "Prague",
-  startTime: "2017-06-10T11:00:00.000Z",
-  endTime: "2017-06-10T15:00:00.000Z",
-  description: "All activities starts and ends to the Hilton Hotel. \n\nChill out walk\nAn easy, chilled out walk that includes a bit of the Old town, but most importantly a couple of relaxed beers in the Letná park that overlooks the city center of Prague.\n\n\n\nShopping / urban culture walk\nAn easy walk that goes through the former industrial part of Prague and takes the people to do some shopping in Vnitroblock, a cool shopping area comprised of designer and handcraft-oriented shops.\n\n\n\nBeer walk\nA very easy walk for beer lovers and those, who like to eat 'beer' dishes.\n\n\n\nFootball match\nRuthless and sweaty football match in short walking distance from Hilton Hotel. You can sign up for a player or as a fan. Notice that in that field you cant play with football shoes and you have play with smooth soles. So keep this in mind when doing the packing. The match starts at 13.00 and we have the field until 15.00.",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.093277,
-    longitude: 14.4376183
+  "name": "Activities",
+  "locationName": "Prague",
+  "startTime": "2017-06-10T10:00:00.000Z",
+  "endTime": "2017-06-10T14:00:00.000Z",
+  "description": "All activities starts and ends to the Hilton Hotel. \n\nChill out walk\nAn easy, chilled out walk that includes a bit of the Old town, but most importantly a couple of relaxed beers in the Letná park that overlooks the city center of Prague.\n\n\n\nShopping / urban culture walk\nAn easy walk that goes through the former industrial part of Prague and takes the people to do some shopping in Vnitroblock, a cool shopping area comprised of designer and handcraft-oriented shops.\n\n\n\nBeer walk\nA very easy walk for beer lovers and those, who like to eat 'beer' dishes.\n\n\n\nFootball match\nRuthless and sweaty football match in short walking distance from Hilton Hotel. You can sign up for a player or as a fan. Notice that in that field you cant play with football shoes and you have play with smooth soles. So keep this in mind when doing the packing. The match starts at 13.00 and we have the field until 15.00.",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.093277",
+    "longitude": "14.4376183"
   },
-  coverImage: "https://images.unsplash.com/photo-1494778696781-8f23fd5553c4?dpr=1&auto=compress,format&fit=crop&w=376&h=302&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://images.unsplash.com/photo-1494778696781-8f23fd5553c4?dpr=1&auto=compress,format&fit=crop&w=376&h=302&q=80&cs=tinysrgb&crop=&bg="
 },
 {
-  name: "Summer Party",
-  locationName: "Mánes Building",
-  startTime: "2017-06-10T17:00:00.000Z",
-  endTime: "2017-06-10T22:00:00.000Z",
-  description: "Masarykovo nábřeží 250/1\n110 00 Praha 1\n\n\nhttp://www.manesrestaurant.cz/en",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.0775102,
-    longitude: 14.4119284
+  "name": "Dinner",
+  "locationName": "Mánes Building",
+  "startTime": "2017-06-10T16:00:00.000Z",
+  "endTime": "2017-06-10T19:00:00.000Z",
+  "description": "Dinner in Mánes before Summer Party. \n\nMasarykovo nábřeží 250/1\n110 00 Praha 1",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.0775102",
+    "longitude": "14.4119284"
   },
-  coverImage: "https://images.unsplash.com/photo-1458891216473-4f26bb4eb40e?dpr=1&auto=compress,format&fit=crop&w=376&h=251&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://media-cdn.tripadvisor.com/media/photo-s/09/40/9c/21/manes-group.jpg"
 },
 {
-  name: "Night Snack",
-  locationName: "Mánes Building",
-  startTime: "2017-06-10T22:00:00.000Z",
-  endTime: "2017-06-10T23:00:00.000Z",
-  description: "Everybody must be hungry by now. Prepare yourself for the rest of the night!",
-  organizer: "Futurice",
-  contactDetails: "Kristiina",
-  location: {
-    latitude: 50.0775102,
-    longitude: 14.4119284
+  "name": "Summer Party",
+  "locationName": "Mánes Building",
+  "startTime": "2017-06-10T16:00:00.000Z",
+  "endTime": "2017-06-10T21:00:00.000Z",
+  "description": "Epic Summer Party in Mánes Building. We are having a dinner before the Party. \n\nAddress: \nMasarykovo nábřeží 250/1\n110 00 Praha 1\n\n\nhttp://www.manesrestaurant.cz/en",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.0775102",
+    "longitude": "14.4119284"
   },
-  coverImage: "https://images.unsplash.com/photo-1438001394654-f4837ea93ea2?dpr=1&auto=compress,format&fit=crop&w=376&h=251&q=80&cs=tinysrgb&crop=&bg="
+  "coverImage": "https://images.unsplash.com/photo-1458891216473-4f26bb4eb40e?dpr=1&auto=compress,format&fit=crop&w=376&h=251&q=80&cs=tinysrgb&crop=&bg="
+},
+{
+  "name": "Night Snack",
+  "locationName": "Mánes Building",
+  "startTime": "2017-06-10T21:00:00.000Z",
+  "endTime": "2017-06-10T22:00:00.000Z",
+  "description": "Everybody must be hungry by now. Prepare yourself for the rest of the night!",
+  "organizer": "Futurice",
+  "contactDetails": "Kristiina",
+  "location": {
+    "latitude": "50.0775102",
+    "longitude": "14.4119284"
+  },
+  "coverImage": "https://images.unsplash.com/photo-1438001394654-f4837ea93ea2?dpr=1&auto=compress,format&fit=crop&w=376&h=251&q=80&cs=tinysrgb&crop=&bg="
 }
 ];
